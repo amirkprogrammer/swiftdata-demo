12bc0e106ef6a5dc332ec742fd8896543fbd19c8
amirkprogrammer <amir.m.kabiri@gmail.com>
2024-02-02 12:37:43 -0800

    - add crud operations

--- swiftdata-demo/ContentView.swift
+++ swiftdata-demo/ContentView.swift
@@ -12,12 +12,34 @@ struct ContentView: View {
     
     @Environment(\.modelContext) private var context
     
+    // retrieve all of one data type from the db
+    @Query private var items: [DataItem]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            List(items) { item in
+                HStack {
+                    Text(item.creationDate.description)
+                    Button("Update") {
+                        // update an item from the db
+                        item.creationDate = Date()
+                    }
+                }
+                .swipeActions {
+                    Button("Delete") {
+                        // delete an item from the db
+                        context.delete(item)
+                    }
+                }
+            }
+            
+            Button("Add") {
+                // create a new data item
+                let data = DataItem()
+                
+                // add it to the db
+                context.insert(data)
+            }
         }
         .padding()
     }
